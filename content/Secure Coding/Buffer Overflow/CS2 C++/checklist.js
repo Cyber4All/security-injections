640026f5d137c543c16528af8468366204b8f7a0
Shannon Ioffe <109975145+shanioffe@users.noreply.github.com>
2023-01-26 11:32:53 -0500

    Fixes code checklist in security injections

--- content/Secure Coding/Buffer Overflow/CS2 C++/checklist.js
+++ content/Secure Coding/Buffer Overflow/CS2 C++/checklist.js
@@ -1,166 +1,214 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  
 	var name = "Pgm1CL";
-
 	// track id of the current checklist task (initialized later)
 	var current;
 	// track which spans have been clicked appropriately
 	var clicked = [];
 	// track which spans are needed for current question
 	var waitingOn = [];
-
+	// collects total num spans per question
 	var spans = [];
-
+	// helps reset prog bar for each question
+	var previousWaitingOnCount = 0;
+  
 	/**
 	 * returns the id of the next question, and rearranges class indicators
 	 * if question is not provided, initialize the first question
 	 */
 	function advance(question) {
 		var next;
-		if(typeof(question)==='undefined') {
-			next = name+"-array-dec";				// first question
-		} else {
-			// check off question
-			$("#"+question).prop('checked', true);
 
+		// remove highlight from previous question
+		$(".span-" + name).removeClass("si-code-clicked");
 
-
-						// take focus away from current question
-						$("#"+question+"-label").removeClass("si-checklist-active");
-
-
-						switch(question) { // here's where flow is really controlled
-						case name+"-array-dec":		next = name+"-array-ref";	break;
-						case name+"-array-ref":	next = name+"-var-range";	break;
-						case name+"-var-range":		next = name+"-var-occur";	break;
-						case name+"-var-occur":	next = name+"-var-modify";	break;
-						case name+"-var-modify":	next = name+"-var-array";	break;
-						case name+"-var-array":	next = name+"-loop-limit";	break;
-						case name+"-loop-limit":	next = name+"-loop-range";	break;
-						case name+"-loop-range":	next = name+"-function-index";	break;
-						case name+"-function-index":	next = name+"-function-loop";	break;
-						case name+"-function-loop":	next = name+"-function-range";	break;
-						case name+"-function-range":	next = name+"-function-call";	break;
-
-						default:	return;	// input invalid or question is complete
-						}
-					}
-
-					// focus on next question
-					$("#"+next+"-label").addClass("si-checklist-active");
-
-				if((next === name+"-var-range") ||(next === name+'-loop-range') || (next===name +'-function-range')){ //IF POPOVER IS NEEDED
-					waitingOn = []; //Gather spans
-					$(".span-"+name).each(function(index) {
-						if( $(this).hasClass(next+"-"+name) ) {
-							waitingOn.push(index);
-						}
-					});
-					var spanLoc;
-					var spanLoc2;
-					if(next === name+"-var-range"){
-						spanLoc = 20;
-						spanLoc2 = 45;
-					} //NEED TO BE SET TO THE RIGHT SPAN 45
-					else if (next === name+'-loop-range'){
-						spanLoc = 15;
-						spanLoc2 = 37;
-					}//37
-					else if (next === name+'-function-range'){
-						spanLoc = 9;
-						spanLoc2 = 31;
-					}//31
-
-					//show Popover over the index span
-					showPopOver(spans[spanLoc]);
-					times = 0;
-
-					$('input[name=popSave2]').on('click', function() {
-						if ($('#lowInt').val() == "0" && $('#popSelect1').val() == "<="
-						 	&& $('#popSelect2').val() == "<" && $('#highInt').val() == "10"){//these need to change to the right answer for the question
-							//$("#"+tag).prop('checked', true); //not needed because click event handles this.
-							if(times == 0){
-								resetPopOver();
-								times = 1;
-								showPopOver(spans[spanLoc2]);
-							}
-							else{
-								resetPopOver();
-								$(".span-"+name).click(); //click the empty span so that next question is asked.
-
-							}
-						}
-						else{
-							//$('.popover').css('background-color', 'lightpink');
-						}
-
-					});
-
-
-				}
-
-				else{ //normal question
-
-					waitingOn = [];
-					$(".span-"+name).each(function(index) {
-						if( $(this).hasClass(next+"-"+name) ) {
-							waitingOn.push(index);
-						}
-					});
-				}
-
-					return next;
-				}
-
-
-				current = advance();
-
-
-	$(".span-"+name).each(function(index) {
-		spans.push($(this));
-		//console.log(spans);
-		$(this).on('click', function() {
-
-			span = $(this);
-			// continue if user is currently supposed to click this span, and hasn't already
-			if( span.hasClass(current+"-"+name) && $.inArray(index,clicked) < 0 ) {
-				clicked.push(index);					// 		note that it's been clicked, programmatically
-				span.addClass("si-code-clicked");		//		note that it's been clicked, graphically
-				if(span.hasClass(name+"-var-modify-"+name)|| span.hasClass(name+"-var-array-"+name)) { //needs to be changed for vulnerabilities
-					span.addClass("si-code-vulnerability"); //	some spans get extra graphics to indicate vulnerability
-				}
-
-
-				// Check if 'current' question is finished yet
-				var finished = true;
-				for(i in waitingOn) {
-					finished &= $.inArray(waitingOn[i],clicked) >= 0;
-				}
-
-				// if it is, go to next question
-				if(finished) current = advance(current);
+		if (typeof question === "undefined") {
+			next = name + "-array-dec"; // first question
+		 } else {
+			$("#" + question).prop("checked", true); // check off question
+  
+			// timeout allows progress bar to show when full
+			setTimeout(function(){
+				// take focus away from current question
+				$("#"+question+"-label").removeClass("si-checklist-active");
+				$("#" + question + "-progress-label").addClass("progress-hidden");
+				$("#" + question + "-progress-label").removeClass("progress");
+			}, 500);
+  
+			switch (question) { // here's where flow is really controlled
+			case name + "-array-dec":
+				next = name + "-array-ref";
+				break;
+			case name + "-array-ref":
+				next = name + "-var-range";
+				break;
+			case name + "-var-range":
+				next = name + "-var-occur";
+				break;
+			case name + "-var-occur":
+				next = name + "-var-modify";
+				break;
+			case name + "-var-modify":
+				next = name + "-var-array";
+				break;
+			case name + "-var-array":
+				next = name + "-loop-limit";
+				break;
+			case name + "-loop-limit":
+				next = name + "-loop-range";
+				break;
+			case name + "-loop-range":
+				next = name + "-function-index";
+				break;
+			case name + "-function-index":
+				next = name + "-function-loop";
+				break;
+			case name + "-function-loop":
+				next = name + "-function-range";
+				break;
+			case name + "-function-range":
+				next = name + "-function-call";
+				break;
+	
+			default:
+				return; // input invalid or question is complete
 			}
+		}
+  
+		// focus on next question
+		$("#" + next + "-label").addClass("si-checklist-active");
+		// remove hidden class from progress bar when user clicks correct answer
+		$("#" + next + "-progress-label").removeClass("progress-hidden");
+		// add the progress bar fill
+		$("#" + next + "-progress-label").addClass("progress");
+  
+		//IF POPOVER IS NEEDED
+		if (
+			next === name + "-var-range" ||
+			next === name + "-loop-range" ||
+			next === name + "-function-range"
+		) {
+
+			// remove progress bar
+			if (next.endsWith("var-occur") || next.endsWith("var-range") || next.endsWith("-loop-range")) {
+				$("#"+next+"-progress-label").removeClass("progress");
+				$("#"+next+"-progress-label").addClass("progress-hidden");
+			}
+	
+			waitingOn = []; // gather spans remaining to be clicked
+	
+			$(".span-" + name).each(function (index) {
+			if ($(this).hasClass(next + "-" + name)) {
+				waitingOn.push(index);
+			}
+			});
+	
+			var spanLoc;
+			var spanLoc2;
+			if (next === name + "-var-range") {
+			spanLoc = 20;
+			spanLoc2 = 45;
+			} //NEED TO BE SET TO THE RIGHT SPAN 45
+			else if (next === name + "-loop-range") {
+			spanLoc = 15;
+			spanLoc2 = 37;
+			} //37
+			else if (next === name + "-function-range") {
+			spanLoc = 9;
+			spanLoc2 = 31;
+			} //31
+	
+			//show Popover over the index span
+			showPopOver(spans[spanLoc]);
+			times = 0;
+	
+			$("input[name=popSave2]").on("click", function () {
+			if (
+				$("#lowInt").val() == "0" &&
+				$("#popSelect1").val() == "<=" &&
+				$("#popSelect2").val() == "<" &&
+				$("#highInt").val() == "10"
+			) {
+				//these need to change to the right answer for the question
+				if (times == 0) {
+				resetPopOver();
+				times = 1;
+				showPopOver(spans[spanLoc2]);
+				} else {
+				resetPopOver();
+				$(".span-" + name).click(); //click the empty span so that next question is asked.
+				}
+			} else {
+				//$('.popover').css('background-color', 'lightpink');
+			}
+			});
+		} else { // NORMAL QUESTION
+  
+		waitingOn = [];
+		$(".span-" + name).each(function (index) {
+		  if ($(this).hasClass(next + "-" + name)) {
+			waitingOn.push(index);
+		  }
 		});
+	  }
+	  return next;
+	}
+  
+	current = advance();
+  
+	// Handles score for each individual question
+	$(".span-" + name).each(function (index) {
+	  spans.push($(this));
+	  
+	  $(this).on("click", function () {
+		span = $(this);
+		
+		// continue if user is currently supposed to click this span, and hasn't already
+		if(span.hasClass(current+"-"+name) && $.inArray(index,clicked) < 0 ) {
+			clicked.push(index);	// 	note that it's been clicked, programmatically
+			span.addClass("si-code-clicked");	//	note that it's been clicked, graphically
+
+			// logic for increasing the progress bar
+			let currentProgress = ((clicked.length - previousWaitingOnCount)/waitingOn.length)*100;
+			document.getElementById(current + "-progress-data-label").style.width = `${currentProgress}%`;
+
+			// Check if 'current' question is finished yet
+			var finished = true;
+			for(i in waitingOn) {
+				finished &= $.inArray(waitingOn[i],clicked) >= 0;
+			}
+
+			// if it is, go to next question
+			if (finished) {
+				// highlights correct answer before moving on to next question
+				setTimeout(function(){
+					previousWaitingOnCount += waitingOn.length;
+					current = advance(current);
+				}, 500);
+			}
+		}
+	  });
 	});
-
-	function resetPopOver(){
-		$('.rangepop').hide();
-		$('#lowInt').val('');
-		$('#popSelect1').val('<');
-		$('#popSelect2').val('<');
-		$('#highInt').val('');
-		$('.popover').css('background-color', 'white');
+  
+	function resetPopOver() {
+	  $(".rangepop").hide();
+	  $("#lowInt").val("");
+	  $("#popSelect1").val("<");
+	  $("#popSelect2").val("<");
+	  $("#highInt").val("");
+	  $(".popover").css("background-color", "white");
 	}
-
-	function showPopOver(span) { //showsPopover over span
-	    var offset = span.offset();
-	    var position = span.position();
-	    var theHeight = $('.popover').height();
-	    $('.rangepop').show();
-			$(".popover").css({
-             'position': 'absolute',
-             'top': (position.top-(theHeight/2)-5) + 'px',
-             'left': (position.left+50) + 'px'
-       });
+  
+	//showsPopover over span
+	function showPopOver(span) {
+	  var offset = span.offset();
+	  var position = span.position();
+	  var theHeight = $(".popover").height();
+	  $(".rangepop").show();
+	  $(".popover").css({
+		position: "absolute",
+		top: (position.top - (theHeight / 2) - 5) + "px",
+		left: (position.left + 100) + "px",
+	  });
 	}
-
-});
+  });
--- content/Secure Coding/Encapsulation/CS2 C++/checklist.js
+++ content/Secure Coding/Encapsulation/CS2 C++/checklist.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
+	
 	var name = "Pgm1CL";
-
 	// track id of the current checklist task (initialized later)
 	var current;
 	// track which spans have been clicked appropriately
 	var clicked = [];
 	// track which spans are needed for current question
 	var waitingOn = [];
-
+	// helps reset prog bar for each question
+	var previousWaitingOnCount = 0;
 
 	/**
 	 * returns the id of the next question, and rearranges class indicators
@@ -15,26 +16,42 @@ $(document).ready(function() {
 	 */
 	function advance(question) {
 		var next;
+
+		// remove highlight from previous question
+		$(".span-" + name).removeClass("si-code-clicked");
+
 		if(typeof(question)==='undefined') {
-			next = name+"-list-var";				// first question
+			next = name+"-list-var"; // first question
 		} else {
-			// check off question
-			$("#"+question).prop('checked', true);
+			$("#"+question).prop('checked', true); // check off question
 
-
-			// take focus away from current question
-			$("#"+question+"-label").removeClass("si-checklist-active");
-
+			// timeout allows progress bar to show when full
+			setTimeout(function(){
+				// take focus away from current question
+				$("#"+question+"-label").removeClass("si-checklist-active");
+				$("#" + question + "-progress-label").addClass("progress-hidden");
+				$("#" + question + "-progress-label").removeClass("progress");
+			}, 500);
 
 			switch(question) { // here's where flow is really controlled
-			case name+"-list-var":		next = name+"-list-access";	break;
-			case name+"-list-access":	next = name+"-list-modify";	break;
-			default:	return;	// input invalid or question is complete
+				case name+"-list-var":		
+					next = name+"-list-access";	
+					break;
+				case name+"-list-access":	
+					next = name+"-list-modify";	
+					break;
+
+				default:	
+				return;	// input invalid or question is complete
 			}
 		}
 
 		// focus on next question
-		$("#"+next+"-label").addClass("si-checklist-active");
+		$("#" + next + "-label").addClass("si-checklist-active");
+		// remove hidden class from progress bar when user clicks correct answer
+		$("#" + next + "-progress-label").removeClass("progress-hidden");
+		// add the progress bar fill
+		$("#" + next + "-progress-label").addClass("progress");
 
 		// track which spans are needed for next question
 		waitingOn = [];
@@ -43,36 +60,40 @@ $(document).ready(function() {
 				waitingOn.push(index);
 			}
 		});
-
 		return next;
 	}
-
-
+	
 	current = advance();
 
-
-
+	// Handles score for each individual question
 	$(".span-"+name).each(function(index) {
 
 		$(this).on('click', function() {
 			span = $(this);
 
 			// continue if user is currently supposed to click this span, and hasn't already
-			if( span.hasClass(current+"-"+name) && $.inArray(index,clicked) < 0 ) {
-				clicked.push(index);					// 		note that it's been clicked, programmatically
-				span.addClass("si-code-clicked");		//		note that it's been clicked, graphically
-				if(span.hasClass(name+"-list-modify-"+name)) {
-					span.addClass("si-code-vulnerability"); //	some spans get extra graphics to indicate vulnerability
-				}
+			if(span.hasClass(current+"-"+name) && $.inArray(index,clicked) < 0 ) {
+				clicked.push(index);	// 	note that it's been clicked, programmatically
+				span.addClass("si-code-clicked");	//	note that it's been clicked, graphically
 
+				// logic for increasing the progress bar
+				let currentProgress = ((clicked.length - previousWaitingOnCount)/waitingOn.length)*100;
+				document.getElementById(current + "-progress-data-label").style.width = `${currentProgress}%`;
 
 				// Check if 'current' question is finished yet
 				var finished = true;
 				for(i in waitingOn) {
 					finished &= $.inArray(waitingOn[i],clicked) >= 0;
 				}
+
 				// if it is, go to next question
-				if(finished) current = advance(current);
+				if (finished) {
+					// highlights correct answer before moving on to next question
+					setTimeout(function(){
+						previousWaitingOnCount += waitingOn.length;
+						current = advance(current);
+					}, 500);
+				}
 			}
 		});
 	});
